cc9e8ca56634bf41e6f0d919ae664ead07e41cf6
unknown <Ajay Shankar>
2023-02-11 18:46:45 +0530

    pushed to git

--- routes/users.js
+++ routes/users.js
@@ -1,23 +1,46 @@
 const router = require("express").Router();
 const User = require("../models/User");
+const Contact = require("../models/Contact");
 const bycrpt = require("bcrypt");
 
 
-router.get('/', (req, res) =>{
+router.get('/', (req, res) => {
     res.send('welcome to user homepage')
 })
 
 
-router.post('/:email', async(req, res) =>{
-    try{
-        const user = await User.findOne({email: req.body.email});
-        const [password, createdAt, updatedAt, ...others] = user;
-        if(user) res.status(200).json(user);
-        if(!user) res.status(404).json('user not found');
+router.get('/:email', async (req, res) => {
+    try {
+        const user = await User.findOne({ email: req.params.email });
+        if (user) res.status(200).json(user);
+        if (!user) res.status(404).json('user not found');
 
-    }catch(error){
+    } catch (error) {
         res.status(500).json(error);
     }
 
 });
+
+router.post('/contact', async (req, res) => {
+    try {
+        const user = await Contact.findOne({ email: req.body.email });
+        if (!user) {
+            const newContact = new Contact({
+                name: req.body.name,
+                email: req.body.email,
+                phone: req.body.phone,
+                message: req.body.message
+            });
+            // save new contact
+            const contact = await newContact.save();
+            res.status(200).json(contact);
+        } else {
+            res.status(400).json("You query is already being process");
+        }        
+
+    } catch (error) {
+        res.status(500).json('error');
+    }
+});
+
 module.exports = router; 
