29439402e16f14e037b04d1a0d7e04240dcd3751
unknown <Ajay Shankar>
2023-02-11 20:22:13 +0530

    pushed to git

--- index.js
+++ index.js
@@ -33,7 +33,15 @@ app.use("/api/users", userRoute);
 app.use("/api/auth", authRoute);
 app.use("/api/email", emailRoute);
 
-app.use(cors());
+//app.use(cors());
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header(
+      'Access-Control-Allow-Headers',
+      'Origin, X-Requested-With,Content-Type,Accept'
+    );
+    next();
+});
 
 app.get('/', (req, res) => {
     res.send('welcome to homepage')
