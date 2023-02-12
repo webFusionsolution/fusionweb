862ad3d42222fe52e6eae1ec3c4fe23b78a2a364
unknown <Ajay Shankar>
2023-02-12 13:50:00 +0530

    pushed to git

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ app.use(morgan("common"));
 app.use(function (req, res, next) {
 
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', "*");
+    res.setHeader('Access-Control-Allow-Origin', "http://fusionweb.in");
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
